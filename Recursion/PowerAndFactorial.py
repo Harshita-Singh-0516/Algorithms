ba9477caa8f1f486bf0192a4b3c0488a52e5b9e4
Harshita Singh <harshi.singh94@gmail.com>
2020-12-11 00:43:02 +0530

    Create PowerAndFactorial.py

---
+++ Recursion/PowerAndFactorial.py
@@ -0,0 +1,12 @@
+def power(num, pwr)
+  if pwr = 0:
+    return 1
+  else:
+    return num * power(num, pwr - 1)
+    
+    
+def factorial(num)
+  if num = 0:
+    return 1
+  else:
+    return num * factorial(num - 1)
